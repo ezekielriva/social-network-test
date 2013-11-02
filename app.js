e2e32e162292d2b7d7902898040682e0dbb4a13b
ezekielriva <ezerivadeneiral@gmail.com>
2013-11-02 17:11:32 -0300

    Adds file read functionality to server

--- app.js
+++ app.js
@@ -1,14 +1,32 @@
-var http = require('http');
+var http = require('http'),
+		path = require('path'),
+		fs = require('fs');
 
 http.createServer(function(req, res) {
-	var html = '<!DOCTYPE html>' +
-						 '<html><head><title>Hello World</title></head>'+
-						 '<body><h1>Hello, world!</h1></body></html>';
+	var filename = path.basename(req.url) || 'index.html',
+			ext = path.extname(filename),
+			localPath = __dirname + '/public/';
 
-	res.writeHead(200, {
-		"Content-Type": "text/html",
-		"Content-Length": html.length
+	if (ext === '.html') {
+		localPath += filename;
+		path.exists(localPath, function(exists) {
+			if (exists) {
+				getFile(localPath, res);
+			} else {
+				res.writeHead(404);
+				res.end()
+			}
+		});
+	}
+}).listen(3000);
+
+function getFile (localPath, res) {
+	fs.readFile(localPath, function(err, content) {
+		if (!err) {
+			res.end(content);
+		} else {
+			res.writeHead(500);
+			res.end();
+		}
 	});
-
-	res.end(html);
-}).listen(3000, "127.0.0.1");
+}
