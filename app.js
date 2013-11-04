ffa24d9541a28ca00099d1e14bb6d7716ae3c3dd
Ezekiel <ezerivadeneiral@gmail.com>
2013-11-04 14:48:52 -0300

    Adds login basic functionality

--- app.js
+++ app.js
@@ -33,4 +33,24 @@ app.post('/register', function(req, res){
   res.send(200);
 });
 
+app.get('/login', function(req, res) {
+  console.log('login request');
+  var email = req.param('email', undefined),
+      password = req.param('password', undefined);
+
+  if ( !email || email.length < 1 || !password || password.length < 1 ) {
+    res.send(404);
+    return;
+  }
+
+  Account.login(email, password, function(success) {
+    if(!success) {
+      res.send(401);
+      return;
+    }
+    console.log('login was successful');
+    res.send(200);
+  });
+});
+
 app.listen(3000);
