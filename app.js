a42b94e3bbbb8a51b99f98bbea8f98d6dc436d18
Ezekiel <ezerivadeneiral@gmail.com>
2013-11-04 13:58:31 -0300

    Adds account authenticated route

--- app.js
+++ app.js
@@ -10,4 +10,12 @@ app.get('/', function(req, res) {
 	res.render('index', { layout: false });
 });
 
+app.get('/account/authenticated', function(req, res) {
+  if (res.session.loggedIn) {
+    res.send(200);
+  } else {
+    res.send(401);
+  }
+});
+
 app.listen(3000);
